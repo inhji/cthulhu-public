6b7fede4d0bd12a5112629e34dd18542104ddb93
Inhji <johnnie@posteo.de>
2018-02-01 23:13:27 +0100

    format dates & show tags

--- components/post.js
+++ components/post.js
@@ -1,15 +1,25 @@
 import Link from 'next/link'
+import dateformat from 'dateformat'
 
 export default ({ post }) => (
   <article className="post h-entry">
     <p className="p-name e-content">{post.content}</p>
     <p className="meta">
+      <span className="tags">
+        {post.tags.map(tag => (
+          <span>
+            #<a href={`/tag/${tag}`}>{tag}</a>
+          </span>
+        ))}
+      </span>
       <Link
         href={{ pathname: '/note', query: { hashid: post.hashid } }}
         as={`/note/${post.hashid}`}
       >
         <a className="u-url">
-          <time className="dt-published time">{post.createdAt}</time>
+          <time className="dt-published time" dateTime={post.createdAt}>
+            {dateformat(post.createdAt)}
+          </time>
         </a>
       </Link>
       <a u-author href="/" />
