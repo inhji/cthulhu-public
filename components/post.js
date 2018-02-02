2fdd8cf804b2ff3600f2ea8ba4bfb79dcff2b2cb
Inhji <johnnie@posteo.de>
2018-02-02 15:22:05 +0100

    more microformats for notes

--- components/post.js
+++ components/post.js
@@ -1,34 +1,57 @@
 import Link from 'next/link'
 import dateformat from 'dateformat'
 
-export default ({ post }) => (
-  <article className="post h-entry">
-    <p className="p-name e-content">{post.content}</p>
-    <p className="meta">
-      <span className="tags">
-        {post.tags.map(tag => (
-          <span>
-            #<a href={`/tag/${tag}`}>{tag}</a>
-          </span>
-        ))}
+const Tags = ({ tags }) => (
+  <span className="tags">
+    {tags.map(tag => (
+      <span>
+        <span className="hash">#</span>
+        <Link href={`/tag/${tag}`}>
+          <a className="p-category tag">{tag}</a>
+        </Link>{' '}
       </span>
-      <Link
-        href={{ pathname: '/note', query: { hashid: post.hashid } }}
-        as={`/note/${post.hashid}`}
-      >
-        <a className="u-url">
-          <time className="dt-published time" dateTime={post.createdAt}>
-            {dateformat(post.createdAt)}
-          </time>
-        </a>
-      </Link>
-      <a u-author href="/" />
-    </p>
+    ))}
 
     <style jsx>{`
+      .hash,
+      .tag {
+        color: #828282;
+      }
+    `}</style>
+  </span>
+)
+
+const Permalink = ({ date, hashid }) => (
+  <span className="permalink">
+    <Link href={{ pathname: '/note', query: { hashid } }} as={`/note/${hashid}`}>
+      <a className="u-url">
+        <time className="dt-published time" dateTime={date}>
+          {dateformat(date)}
+        </time>
+      </a>
+    </Link>
+
+    <style jsx>{`
+      .permalink {
+        margin-right: 5px;
+      }
+
       time {
         color: #828282;
       }
     `}</style>
+  </span>
+)
+
+const Author = () => <a class="u-author" href="/" />
+
+export default ({ post }) => (
+  <article className="post h-entry">
+    <p className="p-name e-content">{post.content}</p>
+    <p className="meta">
+      <Permalink date={post.createdAt} hashid={post.hashid} />
+      <Tags tags={post.tags} />
+      <Author />
+    </p>
   </article>
 )
