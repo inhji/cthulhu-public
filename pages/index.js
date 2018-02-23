7bac2f51622c5688e93e36c50a3134f114e5e9b2
Inhji <johnnie@posteo.de>
2018-02-23 16:18:02 +0100

    add h-feed class to postlist

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,16 @@ import query from '../queries/index'
 import { execQuery } from '../lib/graphql'
 import Post from '../components/post'
 
-const Index = ({ posts }) => (
-  <Layout title="Home">
-    <div className="posts">{posts.map(post => <Post post={post} />)}</div>
+const Index = ({ posts, tracks }) => (
+  <Layout tracks={tracks}>
+    <data className="p-name" value="" />
+    <div className="posts h-feed">{posts.map(post => <Post post={post} />)}</div>
   </Layout>
 )
 
 Index.getInitialProps = async ({ req }) => {
-  const { posts } = await execQuery(query)
-  return { posts }
+  const { posts, tracks } = await execQuery(query)
+  return { posts, tracks }
 }
 
 export default Index
