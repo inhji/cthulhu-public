15149379ccf7bf670970bce99c5b1031352e155f
Inhji <johnnie@posteo.de>
2018-03-02 22:59:23 +0100

    serve emojis

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const next = require('next')
+const path = require('path')
+const serveStatic = require('serve-static')
 
 const port = 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -11,9 +13,12 @@ const postHandler = (req, res) => {
   return app.render(req, res, '/post', { hashid: req.params.hashid })
 }
 
+console.log(__dirname)
+
 app.prepare().then(() => {
   const server = express()
 
+  server.use('/assets', serveStatic(path.join(__dirname, 'assets')))
   server.get('/note/:hashid', postHandler)
   server.get('/bookmark/:hashid', postHandler)
   server.get('/article/:hashid', postHandler)
