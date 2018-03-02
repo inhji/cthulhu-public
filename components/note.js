c1ba79db754bd7848730fca2660da7064e3bb6a1
Inhji <johnnie@posteo.de>
2018-03-02 22:53:26 +0100

    show content as html

--- components/note.js
+++ components/note.js
@@ -4,7 +4,10 @@ import Author from './author'
 
 const Note = ({ note: { content, hashid, tags, createdAt, type } }) => (
   <article className="post note">
-    <p className="p-name e-content">{content}</p>
+    <p className="p-name e-content">
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </p>
+
     <p className="meta">
       <Permalink date={createdAt} hashid={hashid} type={type} />
       <Tags tags={tags} />
