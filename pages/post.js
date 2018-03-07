f51e2e47351f10abdd26129d472236717a2d0ccb
Inhji <johnnie@posteo.de>
2018-03-07 20:37:26 +0100

    prevent schnack from being loaded in dev mode

--- pages/post.js
+++ pages/post.js
@@ -7,15 +7,25 @@ import { execQuery } from '../lib/graphql'
 class PostPage extends React.Component {
   static async getInitialProps ({ query: { hashid } }) {
     const { postByHashid } = await execQuery(query, { hashid })
-    return { post: postByHashid }
+    const dev = process.env.NODE_ENV !== 'production'
+
+    return { post: postByHashid, dev }
   }
 
   componentDidMount () {
+    // FIXME: Stupid hack to prevent "Illegal document.domain value" in dev mode
+    // This results in comments not being loaded when in dev mode
+    if (this.props.dev) {
+      return
+    }
+
+    /* eslint-disable no-new, no-undef */
     new Schnack({
       target: '.comments',
       slug: this.props.post.hashid,
       host: 'https://schnack.inhji.de'
     })
+    /* eslint-enable no-new, no-undef */
   }
 
   render () {
