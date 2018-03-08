c7ca279e5632a084374d26a3ba32e3130abdd75c
Inhji <johnnie@posteo.de>
2018-03-08 23:21:52 +0100

    add markdown loader OMG THIS IS SO COOL :DDDDD

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
-// next.config.js
+const path = require('path')
 const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+
+module.exports = withSass({
+  webpack (config, options) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: [
+        {
+          loader: 'html-loader'
+        },
+        {
+          loader: 'markdown-loader',
+          options: {
+            gfm: true,
+            breaks: true,
+            xhtml: true
+          }
+        }
+      ]
+    })
+
+    config.resolve.alias['Components'] = path.resolve(__dirname, 'components/')
+    config.resolve.alias['Lib'] = path.resolve(__dirname, 'lib/')
+    config.resolve.alias['Layout'] = path.resolve(__dirname, 'components/layout.js')
+
+    return config
+  }
+})
