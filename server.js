0911be224dd0af1fd1a9a18e5b4ba445f6392b95
Inhji <johnnie@posteo.de>
2018-03-23 17:32:46 +0100

    Revert "this file is only registering babel and starting the server"
    This reverts commit ad47be1cff36692ad983a4fca60e2e36afdb2aea.

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-require('dotenv').config()
-require('babel-polyfill')
-require('babel-register')({
-  presets: ['env'],
-  plugins: ['transform-object-rest-spread']
+const express = require('express')
+const next = require('next')
+const path = require('path')
+const serveStatic = require('serve-static')
+
+const port = 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.use(
+    '/assets',
+    serveStatic(path.join(__dirname, 'assets'), {
+      immutable: true,
+      maxAge: '7d'
+    })
+  )
+
+  server.get('/post/:hashid', (req, res) => {
+    return app.render(req, res, '/post', { hashid: req.params.hashid })
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
-
-const { start } = require('./server/index')
-
-start()
