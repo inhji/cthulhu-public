8198bda3d5edff6feb4ed7c8c8bf55320c5b38e1
Inhji <johnnie@posteo.de>
2018-03-23 17:32:40 +0100

    Revert "add blicke & meine kleine heile welt"
    This reverts commit dc597965909ea71e99f33531b2a1df5724e01f91.

--- pages/gedichte/meine-kleine-heile-welt/index.js
+++
@@ -1,8 +0,0 @@
-import Layout from 'Layout'
-import html from './content.md'
-
-export default () => (
-  <Layout title="Meine kleine heile Welt">
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-  </Layout>
-)
