d21052e03d9450e9bd2596611b755e0bcbfe99b2
Inhji <johnnie@posteo.de>
2018-03-23 17:32:47 +0100

    Revert "add other server files"
    This reverts commit 0316b573d60bee23d2c606839e1b6da58e7c39ae.

--- server/permalinks.js
+++
@@ -1,3 +0,0 @@
-export function generatePermalink ({ hashid, type }) {
-  return `${process.env.PUBLIC_URL}/${type.toLowerCase()}/${hashid}`
-}
--- server/webmention.js
+++
@@ -1,7 +0,0 @@
-import fetch from 'node-fetch'
-
-export async function sendWebmention (from, to) {
-  await fetch(to)
-    .then(res => res.text())
-    .then(body => console.log(body))
-}
